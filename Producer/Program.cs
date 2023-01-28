a6ecbe4724601022aaf41452c204b47148bc2549
Elif Gökpınar <72789565+elifgokpinar@users.noreply.github.com>
2023-01-28 13:24:23 +0300

    first commit

---
+++ Producer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{ HostName = "localhost" };
+//Create connection
+using var connection = factory.CreateConnection();
+//Create channel
+using var channel = connection.CreateModel();
+channel.QueueDeclare(queue: "order", durable:false, exclusive:false, autoDelete:false, arguments: null);
+
+var message = "This is message from producer.";
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+//publish message
+channel.BasicPublish("", "order", null, encodedMessage);
+Console.WriteLine("Message is published");
